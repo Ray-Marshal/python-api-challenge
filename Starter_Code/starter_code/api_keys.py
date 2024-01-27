81b2a8415fe931718630a5349e17f34c4dd54078
Marshal Rittenger <mrrittenger@gmail.com>
2024-01-27 11:48:46 -0800

    WeatherPy Finished Minus Analyses

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "aab37b9f99bfe57e8a4c63f7ab96e074"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5a75ba364f12462b8220f2f2388ed8c0"
